a73fdc337e25231617e4d6850d6ca251add911a1
Ajay Mishra <mishraka@gmail.com>
2015-11-23 00:07:13 -0800

    Landmark detector integrated into the system

--- findObjects.php
+++ findObjects.php
@@ -1,6 +1,15 @@
 <?php
-// $output = shell_exec('ls -lart');
 $image_name = $_GET["image_name"];
-//echo "argument is $image_name <br>";
-echo "<img src=\"".$image_name."\" alt=\"dummy2.jpg\"  style=\"max-height: 640px; max-width: 640px;\" />";
+$cmd = "/var/www/html/visual_landmark_detector ".$image_name;
+$output = shell_exec($cmd);
+#echo "command to execute:". $cmd;
+$path_parts = pathinfo($image_name);
+$result_image_file = $path_parts['dirname']."/".$path_parts['filename']."_results.".$path_parts['extension'];
+#echo "result image filename is $result_image_file";
+if (file_exists($result_image_file)){
+	echo "<br><img src=\"".$result_image_file."\" alt=\"dummy2.jpg\"  style=\"max-height: 640px; max-width: 640px;\" />";
+}
+else{
+	echo "<br>Image couldn't be processed correctly!!";
+}
 ?>
--- upload.php
+++ upload.php
@@ -28,7 +28,7 @@ $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
 if(isset($_POST["submit"])) {
     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
     if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
+        #echo "File is an image - " . $check["mime"] . ".";
         $uploadOk = 1;
     } else {
         echo "File is not an image.";
@@ -57,7 +57,7 @@ if ($uploadOk == 0) {
 // if everything is ok, try to upload file
 } else {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.<br>";
+        #echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.<br>";
 	echo "<img src=\" " . $target_file . " \" alt=\"dummy.jpg\" style=\"max-height: 640px; max-width: 640px;\" />"; 
 	echo "<br><button onclick=\"findObjects('".$target_file."');\"> Find Me Objects </button>";
     } else {
