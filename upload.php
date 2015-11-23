9738eb645f8ba11157423801985aa8dad60500cd
Ajay Mishra <mishraka@gmail.com>
2015-11-23 08:21:43 +0000

    Accepts images with .JPG extension now

--- upload.php
+++ upload.php
@@ -46,7 +46,7 @@ if ($_FILES["fileToUpload"]["size"] > 5000000) {
     $uploadOk = 0;
 }
 // Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+if($imageFileType != "jpg" && $imageFileType != "JPG" && $imageFileType != "png" && $imageFileType != "jpeg"
 && $imageFileType != "gif" ) {
     echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
     $uploadOk = 0;
